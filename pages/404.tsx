cd0c9ef3aec02d56c1867a9ed48220ece2aad1c0
jooyeal <34053954+jooyeal@users.noreply.github.com>
2022-03-16 11:40:24 +0900

    커스텀404페이지 추가, 레이아웃을 전역에서 제거

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const Custom404 = (props: Props) => {
+  return <div>404</div>;
+};
+
+export default Custom404;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "../components/common/Layout";
 import { store } from "../store";
 import { Provider } from "react-redux";
-import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
   return (
     <Provider store={store}>
-      {router?.pathname == "/login" || router?.pathname == "/signUp" ? (
-        <Component {...pageProps} />
-      ) : (
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      )}
+      <Component {...pageProps} />
     </Provider>
   );
 }
